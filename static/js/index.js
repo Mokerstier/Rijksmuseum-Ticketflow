f9a4f8ff26a4011c9bd574da627c461de26c6d33
Mokerstier <pannekoek-wouter@hotmail.com>
2020-05-20 18:56:33 +0200

    validation error first-form

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,6 @@
 (function(){
-    const firstForm = document.querySelector('form')
+    const firstForm = document.querySelector('.form-first-step')
+    const validationError = document.querySelector('.field-validation-error')
 
     firstForm.addEventListener('change', function(e){
         const inputs = firstForm.querySelectorAll('select')
@@ -17,16 +18,15 @@
             return value
         })
         
-        const sumOfOptions = optionValues.reduce((current, all) =>{
+        const totalArticles = optionValues.reduce((current, all) =>{
             all = current + all
             return all
         })
-        console.log(sumOfOptions)
-
-        if(sumOfOptions >= maxAmountOfArticles){
-            console.log('Show link to group tickets')
-            console.log(maxAmountOfArticles+ ' is the maximum amount')
-            
+        console.log(totalArticles)
+        if(totalArticles >= maxAmountOfArticles){    
+            validationError.classList.remove('hidden')
+        } else{
+            validationError.classList.add('hidden')
         }
     })
     
