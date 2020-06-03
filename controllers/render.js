a2f32a4214746b9404fab73881910bf516a9b56c
MohamadAlGhorani <moh.gh266@gmail.com>
2020-06-03 15:50:38 +0200

    first and second step

--- controllers/render.js
+++ controllers/render.js
@@ -1,7 +1,21 @@
 const getter = require('./getter.js')
 const ticketShopJSON = require('../data/ticketshop-configuration.json')
+let ticketChoice = null
+let ticketDefault = []
+let groupChoice = ""
+
+function checkDefault() {
+    if (groupChoice === "small-group") {
+        return ['0', '0', '0', '0', '0', '0']
+    } else if (groupChoice === "family") {
+        return ['2', '2', '0', '0', '0', '0']
+    } else if (groupChoice === "date") {
+        return ['2', '0', '0', '0', '0', '0']
+    }
+}
 
 function getFirstStep(req, res) {
+    ticketChoice = null
     res.render('pages/firstStep.ejs', {
         title: 'Kies je groep',
     })
@@ -12,19 +26,21 @@ function getSecondStep(req, res) {
     const variantContent = ticketShopJSON.variantContent[0]
     const articleConfiguration = ticketShopJSON.articleConfiguration[0]
     groupChoice = req.query.groupChoice
+    ticketDefault = checkDefault()
+    console.log(ticketDefault)
     if (groupChoice === "large-group") {
         res.redirect('https://www.rijksmuseum.nl/nl/groepsbezoek')
-    } else {
-        res.render('pages/secondStep.ejs', {
-            title: 'Ticket keuze',
-            articles: articles,
-            variantContent: variantContent,
-            articleConfiguration: articleConfiguration,
-            groupChoice: groupChoice,
-            formName: `${groupChoice}TicketChoice`
-        })
     }
-
+    res.render('pages/secondStep.ejs', {
+        title: 'Ticket keuze',
+        articles: articles,
+        variantContent: variantContent,
+        articleConfiguration: articleConfiguration,
+        groupChoice: groupChoice,
+        formName: `${groupChoice}TicketChoice`,
+        ticketChoice: ticketChoice,
+        ticketDefault: ticketDefault
+    })
 }
 
 function getThirdStep(req, res) {
@@ -32,7 +48,14 @@ function getThirdStep(req, res) {
     const ticketConfiguration = ticketShopJSON.variantContent[0]
     const articlesDonation = getter.getDonation()
     const articlesAdditional = getter.getAdditional()
+    if (req.query.Articles) {
+        ticketChoice = req.query.Articles.a
+    }
+    if (req.query.ticketChoice) {
+        ticketChoice = req.query.ticketChoice
+    }
     ticketCount = req.query.totalTickets
+    console.log(ticketChoice)
     res.render('pages/thirdStep.ejs', {
         title: 'Plan je bezoek',
         expositionContents: expositionContents,
