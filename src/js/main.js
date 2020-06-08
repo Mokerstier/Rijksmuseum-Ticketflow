9e98bda30e314faa5b30dcab0b546e6d4ef035a8
MohamadAlGhorani <moh.gh266@gmail.com>
2020-06-08 14:03:00 +0200

    zonder js flow buttons and terug buttons fix

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+(function () {
+    const linkFamily = document.querySelector(".family-flow-link")
+    const linkDate = document.querySelector(".date-flow-link")
+    const linkGroup = document.querySelector(".group-flow-link")
+    if (linkFamily) {
+        linkFamily.href = "/tweede-stap?groupChoice=family&javascript=1";
+        linkDate.href = "/tweede-stap?groupChoice=date&javascript=1";
+        linkGroup.href = "/tweede-stap?groupChoice=small-group&javascript=1";
+    }
+})();
